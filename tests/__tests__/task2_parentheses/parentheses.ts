ece2300497722992645fef78331628f71a3f94df
maxheong54 <maxheong54@gmail.com>
2024-08-05 15:43:13 +0300

    Добавил тесты для выражений со скобками

--- tests/__tests__/task2_parentheses/parentheses.ts
+++ tests/__tests__/task2_parentheses/parentheses.ts
@@ -10,11 +10,14 @@ import { runFile, insp } from '../../helpers/testsHelper';
 
 let pjs = runFile(import.meta.url, 'parentheses.code');
 
-test('result = -6', () => {
-  expect(pjs.engine.results[0]).toBe(-6);
+test('result = 5', () => {
+  expect(pjs.engine.results[0]).toBe(5);
 });
 
-test('result = -5', () => {
-    expect(pjs.engine.results[1]).toBe(-5);
+test('result = 80', () => {
+    expect(pjs.engine.results[1]).toBe(80);
   });
 
+  test('result = 4', () => {
+    expect(pjs.engine.results[2]).toBe(4);
+  });

e85def9bb25b36a29a023fce2ed8926b6e144f4d
maxheong54 <maxheong54@gmail.com>
2024-08-10 08:27:25 +0300

    Удалил лишний закомментированный код

--- src/SyntaxAnalyzer/SyntaxAnalyzer.ts
+++ src/SyntaxAnalyzer/SyntaxAnalyzer.ts
@@ -132,58 +132,6 @@ export class SyntaxAnalyzer {
 	/**
  	*  Разбор "множителя"
  	*/
-	// scanMultiplier(): TreeNodeBase {
-    // 	let integerConstant: SymbolBase | null = this.symbol;
-    //     let parenthesisExpression: TreeNodeBase | null = null;
-
-    //     if (this.symbol !== null && this.symbol.symbolCode === SymbolsCodes.minus) {
-    //         let minus = this.symbol;
-    //         this.nextSym();
-    //         return new UnaryMinus(minus, this.scanMultiplier());
-    //     }
-
-    //     if (this.symbol !== null && this.symbol.symbolCode === SymbolsCodes.openParenthesis) {
-    //         this.nextSym();
-    //         parenthesisExpression = this.scanExpression();
-
-    //         this.accept(SymbolsCodes.closeParenthesis)
-
-    //         return parenthesisExpression;
-    //     }
-
-	// 	if (this.symbol !== null && this.symbol.symbolCode === SymbolsCodes.identifier) {
-	// 		let varName = this.symbol.value;
-	// 		//@ts-ignore
-	// 		let charPosition = this.lexicalAnalyzer.fileIO.charPosition - varName.length;
-	// 		this.nextSym();
-
-	// 		if (varName in this.variables) {
-	// 			return this.variables[varName];
-	// 		}
-
-	// 		// @ts-ignore
-	// 		if (this.symbol !== null && this.symbol.symbolCode === SymbolsCodes.equalSymbol) {
-	// 			this.nextSym();
-	// 			this.variables[varName] = this.scanExpression();
-	// 			//@ts-ignore
-	// 			return new Variable(integerConstant, varName);
-	// 		} else {
-	// 			let lineNumber = (this.symbol !== null &&
-	// 				//@ts-ignore
-	// 				this.symbol.symbolCode === SymbolsCodes.endOfLine) ?
-	// 				this.lexicalAnalyzer.fileIO.lineNumber :
-	// 				this.lexicalAnalyzer.fileIO.lineNumber + 1;
-	// 			let err = `The variable "${varName}" at line ${lineNumber}, position ${charPosition} is not initialized.`;
-	// 			throw err;
-	// 		}
-	// 	}
-
-    // 	this.accept(SymbolsCodes.integerConst); // проверим, что текущий символ это именно константа, а не что-то еще
-
-    // 	return new NumberConstant(integerConstant);
-	// }
-
-
 
 	scanMultiplier(): TreeNodeBase {
     	let integerConstant: SymbolBase | null = this.symbol;
